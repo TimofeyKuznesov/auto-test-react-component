97ac151681095756ab1b33e9928d418c56802d27
Timofey Kuznetsov <t.a.kuznetsov@gmail.com>
2018-03-01 15:18:25 +0300

    add parse conf file

---
+++ .auto-test-react.js
@@ -0,0 +1,12 @@
+import React from "react";
+module.exports = {
+    defaultCortage:{
+        string: ["","string"],
+        number: [-1,0,1],
+        bool: [false,true],
+        func: [()=>{}],
+        array: [[]],
+        object: [{}],
+        element: [<div/>]
+    }
+}
--- src/prop-types-mock.js
+++ src/prop-types-mock.js
@@ -4,27 +4,38 @@
 */
 // eslint-disable-next-line no-unused-vars
 import React from "react";
+import config from "./config";
 var _PropsType = require("prop-types/prop-types");
 var PropsType=Object.assign({},_PropsType);
+
 PropsType.string=_PropsType.string;
-PropsType.string.params=["","string",undefined];
-PropsType.string.isRequired=_PropsType.string.isRequired;
-PropsType.string.isRequired.params=["","string"];
+PropsType.string.isRequired.params=config.defaultCortage.string;
+PropsType.string.params=PropsType.string.isRequired.params.concat([undefined]);
+
 PropsType.number=_PropsType.number;
-PropsType.number.params=[-10,0,10,undefined];
-PropsType.number.isRequired.params=[-10,0,10];
+PropsType.number.isRequired.params=config.defaultCortage.number;
+PropsType.number.params=PropsType.number.isRequired.params.concat([undefined]);
+
 PropsType.bool=_PropsType.bool;
-PropsType.bool.params=[false,true,undefined];
-PropsType.bool.isRequired.params=[false,true];
+PropsType.bool.isRequired.params=config.defaultCortage.bool;
+PropsType.bool.params=PropsType.bool.isRequired.params.concat([undefined]);
+
 PropsType.func=_PropsType.func;
-PropsType.func.params=[()=>{},undefined];
-PropsType.func.isRequired.params=[()=>{}];
+PropsType.func.isRequired.params=config.defaultCortage.func;
+PropsType.func.params=PropsType.func.isRequired.params.concat([undefined]);
+
 PropsType.array=_PropsType.array;
-PropsType.array.params=[[],undefined];
-PropsType.array.isRequired.params=[[]];
+PropsType.array.isRequired.params=config.defaultCortage.array;
+PropsType.array.params=PropsType.array.isRequired.params.concat([undefined]);
+
 PropsType.object=_PropsType.object;
-PropsType.object.params=[{},undefined];
-PropsType.object.isRequired.params=[{}];
+PropsType.object.isRequired.params=config.defaultCortage.object;
+PropsType.object.params=PropsType.object.isRequired.params.concat([undefined]);
+
+PropsType.element=_PropsType.element;
+PropsType.element.isRequired.params=config.defaultCortage.element;
+PropsType.element.params=PropsType.element.isRequired.params.concat([undefined]);
+
 
 PropsType.oneOf=function oneOf(params) {
     let ret = _PropsType.oneOf(params);
@@ -58,8 +69,6 @@ PropsType.arrayOf=function arrayOf(_params) {
     return ret;
 };
 
-PropsType.element=_PropsType.element;
-PropsType.element.isRequired.params=[<div/>];
-PropsType.element.params=PropsType.element.isRequired.params.concat([undefined]);
+
 
 module.exports = PropsType;
